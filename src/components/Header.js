284f21de53a4e4759b552684486b25bff4e8dbdb
shayanafkane <shayanafkane@gmail.com>
2022-01-03 13:51:41 +0330

    Add Social

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,13 @@ function Header() {
     const { colorMode } = useColorMode();
     const isDark = colorMode === "dark";
 
-    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:768px)");
 
 
     return (
 
         <Stack>
-            <Circle position="absolute" bg="blue.100" opacity="0.1" w="400px" h="400px" alignSelf="flex-end" />
+            <Circle position="absolute" bg="blue.100" opacity="0.1" w="300px" h="300px" alignSelf="flex-end" />
             <Flex direction={isNotSmallerScreen ? "row" : "column"} spacing="200px" p={isNotSmallerScreen ? "32" : "0"} >
 
                 <Box mt={isNotSmallerScreen ? "0" : 16} align="flex-end">
@@ -23,7 +23,7 @@ function Header() {
                 </Box>
                 <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"} mb={isNotSmallerScreen ? "0" : "12"}
                     borderRadius="full" backgroundColor="transparent" boxShadow="lg"
-                    boxSize="400px" src='https://res.cloudinary.com/dpzrxnav1/image/upload/v1641201468/test/ezgif.com-gif-maker_11_pznwag.jpg' />
+                    boxSize="300px" src='https://res.cloudinary.com/dpzrxnav1/image/upload/v1641201468/test/ezgif.com-gif-maker_11_pznwag.jpg' />
             </Flex>
         </Stack>
     )
--- src/components/Social.js
+++ src/components/Social.js
@@ -1,9 +1,18 @@
+import { HStack, Icon, useMediaQuery } from '@chakra-ui/react'
 import React from 'react'
-
+import {FaFacebook , FaGoogle , FaSpotify , FaShopify} from 'react-icons/fa';
 function Social() {
+
+
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:768px)");
     return (
         <div>
-            
+            <HStack spacing="24">
+                <Icon as={FaFacebook} boxSize="50" />
+                <Icon as={FaGoogle} boxSize="50" />
+                <Icon as={FaShopify} boxSize="50" />
+                <Icon as={FaSpotify} boxSize="50" />
+            </HStack>
         </div>
     )
 }
