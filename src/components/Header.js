00133e1acb97b5be0ada443cda03e239944bf3e5
shayanafkane <shayanafkane@gmail.com>
2022-01-03 13:22:37 +0330

    Add Flex Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import { Circle, Flex, Stack, useColorMode, useMediaQuery , Box , Text } from '@chakra-ui/react'
+import { Circle, Flex, Stack, useColorMode, useMediaQuery, Box, Text, Button, Image } from '@chakra-ui/react'
 import React from 'react'
 
 function Header() {
@@ -6,25 +6,27 @@ function Header() {
     const { colorMode } = useColorMode();
     const isDark = colorMode === "dark";
 
-    const { isNotSmallerScreen } = useMediaQuery("(mix-width:600px)");
+    const [isNotSmallerScreen] = useMediaQuery("(min-width:600px)");
 
 
     return (
 
         <Stack>
-            <Circle position="absolute" bg="blue.100" opacity="0.1" w="300px" h="300px" alignSelf="flex-end" />
+            <Circle position="absolute" bg="blue.100" opacity="0.1" w="400px" h="400px" alignSelf="flex-end" />
             <Flex direction={isNotSmallerScreen ? "row" : "column"} spacing="200px" p={isNotSmallerScreen ? "32" : "0"} >
 
                 <Box mt={isNotSmallerScreen ? "0" : 16} align="flex-end">
                     <Text fontSize="5xl" fontWeight="semibold">Hi , I am</Text>
                     <Text fontSize="7xl" bgGradient="linear(to-r , cyan.400 , blue.500 , purple.600)" bgClip="text" fontWeight="bold">Shayan Afkane</Text>
+                    <Text color={isDark ? "gray.200" : "gray.500"}>Lorem ipsum dolor sit amet. Est voluptatum rerum sed quos tenetur eum quis officia. Hic tempora omnis sit consectetur aliquid et galisum similique.</Text>
+                    <Button mt={8} colorScheme='blue' onClick={() => window.open("https://shayanafkane.ir")}>Hire Me</Button>
                 </Box>
-
-
-
+                <Image alignSelf="center" mt={isNotSmallerScreen ? "0" : "12"} mb={isNotSmallerScreen ? "0" : "12"}
+                    borderRadius="full" backgroundColor="transparent" boxShadow="lg"
+                    boxSize="400px" src='https://res.cloudinary.com/dpzrxnav1/image/upload/v1641201468/test/ezgif.com-gif-maker_11_pznwag.jpg' />
             </Flex>
         </Stack>
     )
 }
 
-export default Header
+export default Header;
