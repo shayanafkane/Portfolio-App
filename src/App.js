0ff3c1523f7adbf880f753c8c8f59309863cd88a
shayanafkane <shayanafkane@gmail.com>
2022-01-03 12:37:41 +0330

    Add Text

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import { Flex, Heading, IconButton, useColorMode, VStack } from "@chakra-ui/react";
-import { FaMoon, Fainstagram, FaGithub , FaLinkedin, FaSun } from 'react-icons/fa';
+import { Flex, Heading, IconButton, Spacer, useColorMode, VStack } from "@chakra-ui/react";
+import { FaMoon, Fainstagram, FaGithub, FaLinkedin, FaSun, FaInstagram } from 'react-icons/fa';
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Social from "./components/Social";
 function App() {
 
-  const {colorMode , toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const isDark = colorMode === "dark";
 
 
@@ -11,8 +14,16 @@ function App() {
     <VStack p={5}>
       <Flex w="100%">
         <Heading ml="8" size="md" fontWeight="semibold" color="cyan.400">Shayan Afkane</Heading>
+      
+      <Spacer ></Spacer>
+      <IconButton icon={<FaLinkedin />} isRound={true} onClick={toggleColorMode}></IconButton>
+      <IconButton ml="2" icon={<FaInstagram />} isRound={true} onClick={toggleColorMode}></IconButton>
+      <IconButton ml="2" icon={<FaGithub />} isRound={true} onClick={toggleColorMode}></IconButton>
+      <IconButton ml="8" icon={isDark ? <FaSun /> : <FaMoon />} isRound={true} onClick={toggleColorMode}></IconButton>
       </Flex>
-      <IconButton ml="8" icon={isDark ? <FaSun/> : <FaMoon/>} isRound={true} onClick={toggleColorMode}></IconButton>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
     </VStack>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,29 @@
+import { Circle, Flex, Stack, useColorMode, useMediaQuery , Box , Text } from '@chakra-ui/react'
 import React from 'react'
 
 function Header() {
+
+    const { colorMode } = useColorMode();
+    const isDark = colorMode === "dark";
+
+    const { isNotSmallerScreen } = useMediaQuery("(mix-width:600px)");
+
+
     return (
-        <div>
-            
-        </div>
+
+        <Stack>
+            <Circle position="absolute" bg="blue.100" opacity="0.1" w="300px" h="300px" alignSelf="flex-end" />
+            <Flex direction={isNotSmallerScreen ? "row" : "column"} spacing="200px" p={isNotSmallerScreen ? "32" : "0"} >
+
+                <Box mt={isNotSmallerScreen ? "0" : 16} align="flex-end">
+                    <Text fontSize="5xl" fontWeight="semibold">Hi , I am</Text>
+                    <Text fontSize="7xl" bgGradient="linear(to-r , cyan.400 , blue.500 , purple.600)" bgClip="text" fontWeight="bold">Shayan Afkane</Text>
+                </Box>
+
+
+
+            </Flex>
+        </Stack>
     )
 }
 
