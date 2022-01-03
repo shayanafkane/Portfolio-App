3577e84ce99e0080b9113fa55cf29778a9ab1923
shayanafkane <shayanafkane@gmail.com>
2022-01-03 11:44:38 +0330

    Add important components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
